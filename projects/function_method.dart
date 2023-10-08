c6d5e015b98908cc1ac56150abd2a5cc812fb749
Aynur Agayeva <aghayeva552@gmail.com>
2023-10-08 21:46:23 +0400

    function update

--- projects/function_method.dart
+++ projects/function_method.dart
@@ -1,9 +1,8 @@
 void main() {
   printSurname(surname: "Mark");
   printName(name: "John");
-  // List<int> myList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  // List<int> result = myListFunc(myList);
-  // print(result);
+  final result = myListFunc([1, 4, 5, 8, 5, 10]);
+  print(result);
 }
 //10.printSurname adında funksiya yazmaq hansı ki, bizdən String surname istəsin.Həmin surname-i çap etmək
 void printSurname({required String surname}) {
@@ -14,15 +13,28 @@ void printName({required String name}) {
   print(name);
 }
 
+
 //11.
-// myListFunc(List<int> list) {
-// for (int element in list) {
-//     if (element.isEven) {
-//       print(element);
+//  myListFunc(List<int> list) {
+//   for (var index = 0; index < list.length; index++) {
+//     if(list[index].isOdd){
+//       list.remove(list[index]);
 //     }
+
 //   }
- 
+//   return list;
+
 // }
+
+myListFunc(List<int> list) {
+  List<int> evenNum = [];
+  for (var index = 0; index < list.length; index++) {
+    if (list[index].isEven) {
+      evenNum.add(list[index]);
+    }
+  }
+  return evenNum;
+}
 
 //12. funksiya geriyə nəsə qaytarmır, return prosesi dayandırmaq üçün istifadə olunur. Metod isə geriyə datatype qaytarır.
   
